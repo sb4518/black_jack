d065dcadb50ee1c20d06e434f9d7926e8cbb23f4
Samuel Barobhuiya <60411605+sb4518@users.noreply.github.com>
2021-02-17 14:07:20 -0500

    Added three more functions
    convert card function is used to return the value of the car, score() is used to calculate the score the hand, print() to make printing easier when run in the main function

--- blackjack.cpp
+++ blackjack.cpp
@@ -50,5 +50,45 @@ void shuffle(){   //sets all the elements of deck to true
 	for(int i = 0; i < 52;i++)
 		deck[i] = true;
 }
+int convertcard(int card){  //returns the value of the card
+	if(card%13 >= 9)
+		return 10;
+	else if(card%13 < 9 && card%13 != 0)
+		return (card%13)+1;
+	else if(card%13 == 0)
+		return 0;
+}
+//calculates the score of the hand (any hand)
+int score(int hand[],int lastcard){
+	int ace = 0;
+	int total = 0;
+	for(int i = 0; i < lastcard;i++){
+		if(convertcard(hand[i]) != 0)
+			total = total + convertcard(hand[i]);
+		else if(convertcard(hand[i]) == 0)
+			ace++;
+	}
+	for(int i = 0; i < ace;i++){  // checks the ace, will change values if the score becomes higher later on
+		if(total+ 11 <= 21)
+			total = total +11;
+		else if (total+11 > 21)
+			total = total + 1;
+	}
+	return total;
+}
+//prints the player's hand 
+void print(){
+	string rank[13] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+	string suit[4] = {"Spades","Hearts","Clubs","Diamonds"};
+	string DealCard = "",PlayerCard = "";
+	cout<<"Dealer's hand : "<<endl;
+	DealCard = rank[dealhand[0]%13] + " Of " + suit[dealhand[0]/13];
+	cout<<DealCard<<endl;
+	cout<<"Player's hand : "<<endl;
+	for(int i=0;i<plastcard;i++){
+		PlayerCard = rank[p1hand[i]%13] + " Of " + suit[p1hand[i]/13];
+		cout<<"\t "<<PlayerCard<<endl;
+	}
+}
 
 
